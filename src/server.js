bea8cab68122748b1611990931be7a9c69997ed8
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-07-08 16:09:37 -0300

    commit update moogose

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+try {
+     mongoose.connect(process.env.BD_LINK,{
+        useNewUrlParser: true,
+    useUnifiedTopology: true },() => 
+    console.log("Mongoose tá on")
+    );
+
+  } catch (e) {
+    console.log("Mongoose tá off"+e);
+  }
+
+
+
 app.listen(process.env.SERVER_PORT, () => {
     console.log("Servidor está ouvindo?");
-    console.log(process.env.SERVER_PORT);
 }
 )
