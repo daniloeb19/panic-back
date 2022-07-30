8b43cc74099cd8dd56cf80d1f1f42ed231144cc3
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-07-30 13:27:37 -0300

    Ajustes nos Controllers

--- src/controller/MentoradoController.js
+++ src/controller/MentoradoController.js
@@ -134,8 +134,8 @@ module.exports = {
         }
     },
     async updateData(req, res) {
-        const { _id, name, date, pass, email, cpf, seg, contato, sexo, desc } = req.body;
-        const data = { name, date, pass, email, cpf, seg, contato, sexo, desc };
+        const { _id, name, date, email, cpf, seg, contato, sexo, desc } = req.body;
+        const data = { name, date, email, cpf, seg, contato, sexo, desc };
         const user = await Mentorado.findOneAndUpdate({ _id }, data, { new: true });
         if (user == null) {
             return res.json({ erro: "Erro" });
