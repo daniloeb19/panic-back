b8d10fde72db1e862771c8cad60773972bac41ae
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-07-31 02:09:25 -0300

    Update routes.js
    Funcionalidades de mentoria adicionadas

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,29 @@ const router = express.Router();
 const Mentorado = require('./controller/MentoradoController');
 const Mentor = require('./controller/MentorController');
 const Auth = require('./controller/AuthController');
+const Mentoria = require('./controller/MentoriaController');
 const checkToken = require('./middlewares/Jwt');
 
+
+//rotas de autenticação
 router.post('/auth/login', Auth.authLogin);
 router.post('/auth/senha', Auth.authSenha);
 router.post('/auth/senha-log', checkToken, Auth.authSenhaLog);
 router.get('/auth/check', checkToken, async (req, res) => {
     const retorno = await Auth.authCheck({ _id: req.id }, res);
     return retorno;
+});
+
+//rotas de mentoria
+router.post('/api/mentoria', checkToken, async (req, res) => {
+    const retorno = await Mentorado.details({ _id: req.id });
+    const retornoForFront = await Mentoria.create({ _id_mentorado: req.id, _id_mentor: req.body._id_mentor, nome_mentorado: retorno.user.name, contato_mentorado: retorno.user.contato, email_mentorado: retorno.user.email }, res)
+    return retornoForFront;
+
+});
+
+router.get('/api/mentoria', checkToken, async (req, res) => {
+    return await Mentoria.mostrarConexoesMentor({_id_mentor: req.id },res)
 });
 
 //rotas de mentorados
